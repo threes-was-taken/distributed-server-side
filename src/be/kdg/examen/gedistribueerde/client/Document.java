55b6772e27300e4deb8f17b4209b812998feebc4
Threes <driesverelst1998@gmail.com>
2020-11-27 16:21:08 +0100

    Restart

--- src/be/kdg/examen/gedistribueerde/client/Document.java
+++
@@ -1,8 +0,0 @@
-package be.kdg.examen.gedistribueerde.client;
-
-public interface Document {
-    String getText();
-    void setText(String text);
-    void append(char c);
-    void setChar(int position, char c);
-}
